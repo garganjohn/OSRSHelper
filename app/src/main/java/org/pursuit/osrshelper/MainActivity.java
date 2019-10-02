3fbef555bcab5f6d2ec7cb87139f356c49150da7
garganjohn <garganjohn@gmail.com>
2019-10-02 14:25:55 -0400

    added search query

--- app/src/main/java/org/pursuit/osrshelper/MainActivity.java
+++ app/src/main/java/org/pursuit/osrshelper/MainActivity.java
@@ -6,11 +6,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.squareup.picasso.Picasso;
 
 import org.pursuit.osrshelper.network.GEModel;
 import org.pursuit.osrshelper.network.GEService;
@@ -33,25 +28,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initViews();
+        makeCall();
 
-        testBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                itemToBeSearched = itemInput.getText().toString();
-                makeCall(itemToBeSearched);
-            }
-        });
+//        testBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                itemToBeSearched = itemInput.getText().toString();
+//                makeCall();
+//            }
+//        });
 
     }
 
-    public void makeCall(String query) {
+    public void makeCall() {
         Retrofit retrofit = GESingleton.getINSTANCE();
-        Call<GEModel> call = retrofit.create(GEService.class).getWhip(query);
+        Call<GEModel> call = retrofit.create(GEService.class).getSearch("a", 1);
         Log.d(TAG, "onCreate: " + call.request());
         call.enqueue(new Callback<GEModel>() {
             @Override
             public void onResponse(Call<GEModel> call, Response<GEModel> response) {
                 assert response.body() != null;
+                Log.d(TAG, "onResponse: " + response.body());
 
             }
 
--- app/src/main/java/org/pursuit/osrshelper/network/GEService.java
+++ app/src/main/java/org/pursuit/osrshelper/network/GEService.java
@@ -2,13 +2,17 @@ package org.pursuit.osrshelper.network;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface GEService {
     String PATH = "api/catalogue/detail.json";
+    String SEARCH = "api/catalogue/items.json?category=1";
 
     @GET(PATH)
-    Call<GEModel> getWhip(@Query("item") String... itemID);
+    Call<GEModel> getItem(@Query("item") String... itemID);
 
+    @GET(SEARCH)
+    Call<GEModel> getSearch(@Query("alpha") String alpha, @Query("page") int pageNum);
 
 }
