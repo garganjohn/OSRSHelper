e9e7bdbb8563885d60d2875adbdc222a6d213299
garganjohn <garganjohn@gmail.com>
2019-10-22 15:50:49 -0400

    added base components for searchview

--- app/src/main/java/org/pursuit/osrshelper/MainActivity.java
+++ app/src/main/java/org/pursuit/osrshelper/MainActivity.java
@@ -5,19 +5,22 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.SearchView;
 
 
 import org.pursuit.osrshelper.ge_recyclerview.GEAdapter;
 import org.pursuit.osrshelper.item_search.SearchHelper;
 
+import org.pursuit.osrshelper.network.GEModels;
 import org.pursuit.osrshelper.network.NetworkHelper;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
     public static final String TAG = "ur it";
     private NetworkHelper networkHelper;
     private SearchHelper searchHelper;
@@ -36,20 +39,23 @@ public class MainActivity extends AppCompatActivity {
         networkHelper.makeDynamicCall();
 
         searchButton.setOnClickListener(v -> {
-            itemToBeSearched = itemInput.getText().toString();
-            Log.d(TAG, "onCreate: " + searchHelper.userQuery(itemToBeSearched));
-            makeQueryCall(searchHelper.userQuery(itemToBeSearched));
 
-            //Used to close keyboard after enter button has been pressed
-//            try {
-//                InputMethodManager imm = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
-//                imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
-//            } catch (Exception e) {
-//                // TODO: handle exception
-//                Toast.makeText(this, "DEVw", Toast.LENGTH_SHORT).show();
-//            }
         });
 
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+
+        //itemToBeSearched = itemInput.getText().toString();
+        Log.d(TAG, "onCreate: " + searchHelper.userQuery(newText));
+        makeQueryCall(searchHelper.userQuery(newText));
+        return false;
     }
 
     private void initHelpers() {
@@ -67,7 +73,11 @@ public class MainActivity extends AppCompatActivity {
         RecyclerView geRecyclerView = findViewById(R.id.ge_recyclerview);
         geRecyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
         geRecyclerView.setAdapter(geAdapter = new GEAdapter(new ArrayList<>()));
+        SearchView searchView = findViewById(R.id.item_searchview);
+        searchView.setOnQueryTextListener(this);
         searchButton = findViewById(R.id.search_button);
         itemInput = findViewById(R.id.item_input);
     }
+
+
 }
